334d8e1eb1799bbdd3287c20835a532bec54cc6f
Richard Ojo <richardojo04@gmail.com>
2018-07-16 11:54:42 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,11 +1,11 @@
 def alphabetize(arr)
   # code here
   arr.sort do |a, b|
-  if a == b
-    0
-  elsif a > b
-    1
-  elsif a < b
-    -1
-  end
+    if a == b
+      0
+    elsif a > b
+      1
+    elsif a < b
+      -1
+    end
 end
